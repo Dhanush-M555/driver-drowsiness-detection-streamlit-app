14b5b5ab8e21b939608df8a198b7d36378526fde
lash_fire <124171203+Dhanush-M555@users.noreply.github.com>
2024-04-11 17:29:38 +0000

    Resolving audio issues try-12

--- Hello.py
+++ Hello.py
@@ -75,7 +75,8 @@ def drowsiness_detection(frame):
                         t = Thread(target=start_alarm, args=(alarm_sound,))
                         t.daemon = True
                         # st.report_thread.add_report_ctx(t)
-                        st.ReportThread.add_report_ctx(t)
+                        # st.ReportThread.add_report_ctx(t)
+                        st.script_run_context.add_script_run_ctx(t)
                         t.start()
                         cv2.rectangle(roi_color, (x1, y1), (x1 + w1, y1 + h1), (0, 0, 255), 2)  # Red rectangle for closed eyes
             else:
@@ -100,7 +101,8 @@ def drowsiness_detection(frame):
                         t = Thread(target=start_alarm, args=(alarm_sound,))
                         t.daemon = True
                         # st.report_thread.add_report_ctx(t)
-                        st.ReportThread.add_report_ctx(t)
+                        # st.ReportThread.add_report_ctx(t)
+                        st.script_run_context.add_script_run_ctx(t)
                         t.start()
                         cv2.rectangle(roi_color, (x2, y2), (x2 + w2, y2 + h2), (0, 0, 255), 2)  # Red rectangle for closed eyes
             else:
